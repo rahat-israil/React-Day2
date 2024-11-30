d435dbcc4e4b687b7881b75671d5934ca299c232
rahat-israil <rahat.bin.israil@gmail.com>
2024-12-01 01:10:12 +0600

    Event Handeler with Function

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import './App.css'
 
 function App() {
 
+  function handleClick() {      // Normal Function
+    alert('Yes connection successfully')
+  };
+
+  const handleClick2 = () => {      // Arrow Function
+    alert('Button 2 is Clicked')
+  } 
+
   return (
     <>
-      <h1>Day 2 practice</h1>
+      <h1>Day 2 practice</h1>  
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Button 2</button>
+      {/* In line Arrow Function */}
+      <button onClick={() => {alert('Button 3 is Clicked')}}>Button 3</button>    
       
     </>
   )
