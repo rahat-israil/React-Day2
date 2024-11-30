c257b06effae48e428620d081cf710a4b7a83f27
rahat-israil <rahat.bin.israil@gmail.com>
2024-12-01 01:25:20 +0600

    Event handler with parameter functiion

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,19 @@ function App() {
     alert('Button 2 is Clicked')
   } 
 
+  const addToFive = (num) => {
+    alert(num + 5);
+   }
+
   return (
     <>
       <h1>Day 2 practice</h1>  
       <button onClick={handleClick}>Click Me</button>
       <button onClick={handleClick2}>Button 2</button>
       {/* In line Arrow Function */}
-      <button onClick={() => {alert('Button 3 is Clicked')}}>Button 3</button>    
+      <button onClick={() => { alert('Button 3 is Clicked') }}>Button 3</button>    
+      {/* With parameter Function  */}
+      <button onClick={() => addToFive(5)}>Add to 5</button>
       
     </>
   )
